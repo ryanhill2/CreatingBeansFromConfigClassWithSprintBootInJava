799919d8c68fab1a079356ed44a614d5512cccec
ryan <ryan.hill1@mydit.ie>
2021-10-03 14:54:44 +0100

    remove redundant override annotation from IPhone12 & IPhone13 class

--- Artifact/src/main/java/myGroup/myArtifact/IPhone12MobileCPUProcessor.java
+++ Artifact/src/main/java/myGroup/myArtifact/IPhone12MobileCPUProcessor.java
@@ -2,7 +2,6 @@ package myGroup.myArtifact;
 
 public class IPhone12MobileCPUProcessor implements MobileCPUProcessor {
 
-    @Override
     public void DisplayIphoneProcessorType() {
         System.out.println("Iphone 12 CPU Processor");
 
--- Artifact/src/main/java/myGroup/myArtifact/IPhone13MobileCPUProcessor.java
+++ Artifact/src/main/java/myGroup/myArtifact/IPhone13MobileCPUProcessor.java
@@ -2,7 +2,6 @@ package myGroup.myArtifact;
 
 public class IPhone13MobileCPUProcessor implements MobileCPUProcessor{
 
-    @Override
     public void DisplayIphoneProcessorType() {
         System.out.println("Iphone 13 CPU Processor");
     }
